2696b33296d378dcd35af299d19cbd80dfbeb0ac
Jason Tian <jason.tian@pixelforcesystems.com.au>
2020-08-18 11:01:28 +0930

    add platform to app version

--- lib/connector_kit/models/app_version.rb
+++ lib/connector_kit/models/app_version.rb
@@ -3,13 +3,14 @@ require 'connector_kit/models/model'
 module ConnectorKit
   # Simple model class for representing Apps
   class AppVersion < Model
-    attr_reader :app_store_status, :version_string, :all_attrs
+    attr_reader :app_store_status, :version_string, :platform, :all_attrs
 
     def initialize(options)
       super(options)
       attrs = options['attributes']
       @app_store_status = attrs['appStoreState']
       @version_string = attrs['versionString']
+      @platform = attrs['platform']
       @all_attrs = options
     end
   end
